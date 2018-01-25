25ac4eb2b50229cd777d848dd978250be0d11df7
NazguaL <nazgual@ukr.net>
2018-01-25 20:08:49 +0200

    2nd
    2nd

--- src/Implementations.java
+++ src/Implementations.java
@@ -2,5 +2,47 @@
  * Created by User on 24.01.2018.
  */
 public class Implementations {
+    public void printSrtHW () {System.out.println("Hello, World!!!");}
+
+    public int minOfTwo(int a, int b)
+    {
+        if (a < b) return a;
+        else return b;
+    }
+
+    public int summArray(int [] a, int k)
+    {
+        int result = 0;
+        for (int i = 0 ; i < a.length ; i++){
+            if (a[i] == k) {result = result + k;}
+        }
+        return result;
+    }
+
+    public void arrWithZero(int [] a)
+    {
+        int count = 0;
+        for (int i = 0 ; i < a.length ; i++){
+            if (a[i] == 0) {count++;}
+        }
+        if (count > 0) {
+            int[] result = new int[count];
+
+            int index = 0;
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] == 0) {
+                    result[index] = i;
+                    index++;
+                }
+            }
+            System.out.println("There are folowing positions of \"zero\" elements in the array: ");
+            for (int j = 0; j < result.length; j++) {
+                System.out.print((result[j]+1) + " ");
+            }
+        }
+        else {
+            System.out.println("There are no \"zero\" elements in the array!");
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        //TestSyntax
+        /*  class ClassName{
+            public ClassName(){
+            }
+            public static sMethod(){
+            new ClassName().nsMethod();
+            }
+            public nsMethod(){
+            println('non static method')
+            }
+            } */
+        Implementations TestSyntaxCall = new Implementations();
+        TestSyntaxCall.printSrtHW();
+        //minOfTwo
+        int a = 3000;
+        int b = 3001;
+        Implementations miOfTwoCall = new Implementations();
+        System.out.println(miOfTwoCall.minOfTwo(a, b));
+        //summArray
+        int [] c = {20, 15, 30, 30, 40, 30};
+        int k = 30;
+        Implementations summArrayCall = new  Implementations();
+        System.out.println(summArrayCall.summArray(c, k));
+        //arrWithZero
+        int [] d = {20, 0, 15, 30, 30, 40, 30, 0};
+        Implementations arrWithZeroCall = new  Implementations();
+        arrWithZeroCall.arrWithZero(d);
+        //
     }
 }
+
+
+
+
